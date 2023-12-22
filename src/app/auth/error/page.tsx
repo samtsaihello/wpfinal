0c38f183b32e96160274ad0a61fec898e3c26bea
Wesley Chen <1011.5131001w@gmail.com>
2023-12-22 16:07:41 +0800

    feat: try to fix login error

--- src/app/auth/error/page.tsx
+++ src/app/auth/error/page.tsx
@@ -1,28 +1,40 @@
 "use client";
 
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
-import { useSession } from "next-auth/react";
+// import { useSession } from "next-auth/react";
 import { useRouter } from "next/navigation";
 
 import { Toaster } from "@/components/ui/toaster";
 import { useToast } from "@/components/ui/use-toast";
 
 function ErrorPage() {
-  const { data: session } = useSession();
+  // const { data: session } = useSession();
   const router = useRouter();
 
   const { toast } = useToast();
+  const [showToast, setShowToast] = useState(false);
 
   useEffect(() => {
-    toast({
-      title: "Failed",
-      description:
-        "Wrong username or wrong password. Or this username has been used.",
-      variant: "destructive",
-    });
-    router.push("/");
-  }, [session, router, toast]);
+    router.push("/")
+  }, [router]);
+
+  useEffect(() => {
+    return () => {
+      setShowToast(true);
+    };
+  }, []);
+
+  useEffect(() => {
+    if (showToast) {
+      toast({
+        title: "Failed",
+        description:
+          "Wrong username or wrong password. Or this username has been used.",
+        variant: "destructive",
+      });
+    }
+  }, [showToast, toast]);
 
   return (
     <>
