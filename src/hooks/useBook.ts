6ea15436df37bf9067fa3106ba98aed50045f3c7
SAM <samtsai1436@gmail.com>
2023-12-24 01:30:49 +0800

    hook comment

--- src/hooks/useBook.ts
+++ src/hooks/useBook.ts
@@ -1,127 +1,127 @@
-import { useEffect, useMemo, useState } from "react";
+// import { useEffect, useState } from "react";
 
-import { useSession } from "next-auth/react";
-import { useParams, useRouter } from "next/navigation";
+// import { useSession } from "next-auth/react";
+// import { useParams, useRouter } from "next/navigation";
 
-import { useDebounce } from "use-debounce";
+// import { useDebounce } from "use-debounce";
 
-import { pusherClient } from "@/lib/pusher/client";
-import type { Document, User } from "@/lib/types/db";
+// import { pusherClient } from "@/lib/pusher/client";
+// import type { Document, User } from "@/lib/types/db";
 
-type PusherPayload = {
-  senderId: User["id"];
-  document: Document;
-};
+// type PusherPayload = {
+//   senderId: User["id"];
+//   document: Document;
+// };
 
-export const useDocument = () => {
-  const { docId } = useParams();
-  const documentId = Array.isArray(docId) ? docId[0] : docId;
+// export const useDocument = () => {
+//   const { docId } = useParams();
+//   const documentId = Array.isArray(docId) ? docId[0] : docId;
 
-  const [document, setDocument] = useState<Document | null>(null);
-  const [dbDocument, setDbDocument] = useState<Document | null>(null);
-  const router = useRouter();
+//   const [document, setDocument] = useState<Document | null>(null);
+//   const [dbDocument, setDbDocument] = useState<Document | null>(null);
+//   const router = useRouter();
 
-  const { data: session } = useSession();
-  const userId = session?.user?.id;
+//   const { data: session } = useSession();
+//   const userId = session?.user?.id;
 
-  useEffect(() => {
-    const updateDocument = async () => {
-      if (!debouncedDocument) return;
-      // [NOTE] 2023.11.18 - This PUT request will trigger a pusher event that will update the document to the other clients.
-      const res = await fetch(`/api/documents/${documentId}`, {
-        method: "PUT",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-          title: debouncedDocument.title,
-          content: debouncedDocument.content,
-        }),
-      });
-      if (!res.ok) {
-        return;
-      }
-      const data: Document = await res.json();
-      // Update the navbar if the title changed
-      if (debouncedDbDocument?.title !== data.title) {
-        router.refresh();
-      }
-      setDbDocument(data);
-    };
-    updateDocument();
-  }, [debouncedDocument, documentId, router, debouncedDbDocument, isSynced]);
+//   useEffect(() => {
+//     const updateDocument = async () => {
+//       if (!debouncedDocument) return;
+//       // [NOTE] 2023.11.18 - This PUT request will trigger a pusher event that will update the document to the other clients.
+//       const res = await fetch(`/api/documents/${documentId}`, {
+//         method: "PUT",
+//         headers: {
+//           "Content-Type": "application/json",
+//         },
+//         body: JSON.stringify({
+//           title: debouncedDocument.title,
+//           content: debouncedDocument.content,
+//         }),
+//       });
+//       if (!res.ok) {
+//         return;
+//       }
+//       const data: Document = await res.json();
+//       // Update the navbar if the title changed
+//       if (debouncedDbDocument?.title !== data.title) {
+//         router.refresh();
+//       }
+//       setDbDocument(data);
+//     };
+//     updateDocument();
+//   }, [debouncedDocument, documentId, router, debouncedDbDocument, isSynced]);
 
-  // Subscribe to pusher events
-  useEffect(() => {
-    if (!documentId) return;
-    // Private channels are in the format: private-...
-    const channelName = `private-${documentId}`;
+//   // Subscribe to pusher events
+//   useEffect(() => {
+//     if (!documentId) return;
+//     // Private channels are in the format: private-...
+//     const channelName = `private-${documentId}`;
 
-    try {
-      const channel = pusherClient.subscribe(channelName);
-      channel.bind(
-        "doc:update",
-        ({ senderId, document: received_document }: PusherPayload) => {
-          if (senderId === userId) {
-            return;
-          }
-          // [NOTE] 2023.11.18 - This is the pusher event that updates the dbDocument.
-          setDocument(received_document);
-          setDbDocument(received_document);
-          router.refresh();
-        },
-      );
-    } catch (error) {
-      console.error(error);
-      router.push("/docs");
-    }
+//     try {
+//       const channel = pusherClient.subscribe(channelName);
+//       channel.bind(
+//         "doc:update",
+//         ({ senderId, document: received_document }: PusherPayload) => {
+//           if (senderId === userId) {
+//             return;
+//           }
+//           // [NOTE] 2023.11.18 - This is the pusher event that updates the dbDocument.
+//           setDocument(received_document);
+//           setDbDocument(received_document);
+//           router.refresh();
+//         },
+//       );
+//     } catch (error) {
+//       console.error(error);
+//       router.push("/docs");
+//     }
 
-    // Unsubscribe from pusher events when the component unmounts
-    return () => {
-      pusherClient.unsubscribe(channelName);
-    };
-  }, [documentId, router, userId]);
+//     // Unsubscribe from pusher events when the component unmounts
+//     return () => {
+//       pusherClient.unsubscribe(channelName);
+//     };
+//   }, [documentId, router, userId]);
 
-  useEffect(() => {
-    if (!documentId) return;
-    const fetchDocument = async () => {
-      const res = await fetch(`/api/documents/${documentId}`);
-      if (!res.ok) {
-        setDocument(null);
-        router.push("/docs");
-        return;
-      }
-      const data = await res.json();
-      setDocument(data);
-      setDbDocument(data);
-    };
-    fetchDocument();
-  }, [documentId, router]);
+//   useEffect(() => {
+//     if (!documentId) return;
+//     const fetchDocument = async () => {
+//       const res = await fetch(`/api/documents/${documentId}`);
+//       if (!res.ok) {
+//         setDocument(null);
+//         router.push("/docs");
+//         return;
+//       }
+//       const data = await res.json();
+//       setDocument(data);
+//       setDbDocument(data);
+//     };
+//     fetchDocument();
+//   }, [documentId, router]);
 
-  const title = document?.title || "";
-  const setTitle = (newTitle: string) => {
-    if (document === null) return;
-    setDocument({
-      ...document,
-      title: newTitle,
-    });
-  };
+//   const title = document?.title || "";
+//   const setTitle = (newTitle: string) => {
+//     if (document === null) return;
+//     setDocument({
+//       ...document,
+//       title: newTitle,
+//     });
+//   };
 
-  const content = document?.content || "";
-  const setContent = (newContent: string) => {
-    if (document === null) return;
-    setDocument({
-      ...document,
-      content: newContent,
-    });
-  };
+//   const content = document?.content || "";
+//   const setContent = (newContent: string) => {
+//     if (document === null) return;
+//     setDocument({
+//       ...document,
+//       content: newContent,
+//     });
+//   };
 
-  return {
-    documentId,
-    document,
-    title,
-    setTitle,
-    content,
-    setContent,
-  };
-};
+//   return {
+//     documentId,
+//     document,
+//     title,
+//     setTitle,
+//     content,
+//     setContent,
+//   };
+// };
